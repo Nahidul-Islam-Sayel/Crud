eb94e213e24c28bff2bbe3a99a62ec4c638ef697
Nahidul-Islam-Sayel <islamnahidul0825@gmail.com>
2024-01-26 11:15:16 +0600

    first commit

---
+++ Crud.php
@@ -0,0 +1,13 @@
+<?php 
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/*
+ * Plugin Name:CRUD
+ * Description: This is a simple todo plugin.
+ * Version: 1.0.0
+ * Author: Nahidul Islam Sayel
+ * Author URI: http://www.nahidulislamsayel.com
+ */
